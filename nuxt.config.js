3648589f3229069f6137eac0486e8a7082a1b300
Dan <hornedigital01@gmail.com>
2025-01-08 17:00:00 +0000

    push

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,38 @@
+import { fetchStories } from './fetchStories.ts'
+
 export default defineNuxtConfig({
-  css: ['@/assets/css/roboto.css'],
+  hooks: {
+    async 'nitro:config'(nitroConfig) {
+      if (!nitroConfig || nitroConfig.dev) {
+        return
+      }
+      const token = 'XFpR0AEuvI2nLhpnj3F3iwtt'
+ 
+      let cache_version = 0
+ 
+      // other routes that are not in Storyblok with their slug.
+      let routes = ['/'] // adds home directly but with / instead of /home
+      try {
+        const result = await fetch(`https://api.storyblok.com/v2/cdn/spaces/me?token=${token}`)
+ 
+        if (!result.ok) {
+          throw new Error('Could not fetch Storyblok data')
+        }
+        // timestamp of latest publish
+        const space = await result.json()
+        cache_version = space.space.version
+ 
+        // Recursively fetch all routes and set them to the routes array
+        await fetchStories(routes, cache_version)
+       // Adds the routes to the prerenderer
+        nitroConfig.prerender.routes.push(...routes)
+      } catch (error) {
+        console.error(error)
+      }
+    },
+  },
+
+  css: ['@/assets/css/main.css'],
   modules: [
     [
       '@storyblok/nuxt',
@@ -10,6 +43,9 @@ export default defineNuxtConfig({
         }
       },
     ],
-    '@nuxtjs/tailwindcss',
-  ]
+  ],
+  components: {
+    global: true,
+    dirs: ['~/components']
+  },
 })
