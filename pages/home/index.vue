b744660ebef3d0686693edb67607481a9652a1fe
Dan <hornedigital01@gmail.com>
2025-01-08 16:32:34 +0000

    push

---
+++ pages/home/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <StoryblokComponent v-if="story" :blok="story.content" />
+    </div>
+</template>
+
+<script setup>
+    const story = await useAsyncStoryblok('home', { version: 'draft' },
+        { 
+        customParent: 'https://app.storyblok.com' 
+        }
+    )
+
+    useHead({
+      title: 'Connelly Upholstery | The home page',
+      meta: [
+          {
+              name: 'description',
+              content: 'Description'
+          },
+          {
+              property: 'og:title',
+              content: 'Connelly Upholstery | Home pahe'
+          },
+          {
+              property: 'og:description',
+              content: 'Description'
+          },
+      ]
+  })
+</script>
