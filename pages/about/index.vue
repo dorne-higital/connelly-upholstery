f59d9ce01eed1da4b200bb88f6d146c90a345ac6
Dan <hornedigital01@gmail.com>
2025-01-15 19:02:13 +0000

    Update index.vue

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,29 +1,31 @@
 <template>
     <div>
-        <page-hero 
-            :content='{
-                "theme": "dark",
-                "align": "centre",
-                "label": "Connelly Upholstery",
-                "heading": "A little about us",
-                "image": {
-                    "src": imageSrc,
-                    "alt": "Header image of some of our work"
-                }
-            }'
-        />
+        <StoryblokComponent v-if="story" :blok="story.content" />
     </div>
 </template>
-  
-<script>
-    import imageSrc from '~/assets/images/gallery/business_chairs.jpeg';
 
-    export default {
-        data() {
-            return {
-                imageSrc
-            }
+<script setup>
+    const story = await useAsyncStoryblok('about', { version: 'draft' },
+        { 
+        customParent: 'https://app.storyblok.com' 
         }
-    }
+    )
+
+    useHead({
+        title: 'Connelly Upholstery | Get to know us',
+        meta: [
+            {
+                name: 'description',
+                content: 'About page'
+            },
+            {
+                property: 'og:title',
+                content: 'Connelly Upholstery | Get to know us'
+            },
+            {
+                property: 'og:description',
+                content: 'About page'
+            },
+        ]
+    })
 </script>
-  
