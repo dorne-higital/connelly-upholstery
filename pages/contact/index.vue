9d5bfbad25545f85d87327212360c48a7d9102c2
Dan <hornedigital01@gmail.com>
2025-01-16 17:03:59 +0000

    Update index.vue

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
         <StoryblokComponent v-if="story" :blok="story.content" />
+
+        <form name="contact" netlify>
+            <p>
+                <label>Name <input type="text" name="name" /></label>
+            </p>
+            <p>
+                <label>Email <input type="email" name="email" /></label>
+            </p>
+            <p>
+                <button type="submit">Send</button>
+            </p>
+        </form>
     </div>
 </template>
 
