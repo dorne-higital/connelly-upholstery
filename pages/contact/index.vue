55b615b63fbdc3ca31f6a81f51708677fb6c8972
Dan <hornedigital01@gmail.com>
2025-01-16 17:34:08 +0000

    Update index.vue

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -2,33 +2,28 @@
     <div>
         <StoryblokComponent v-if="story" :blok="story.content" />
 
-        <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
-  <!-- Hidden honeypot field to prevent bot submissions -->
-  <input type="hidden" name="form-name" value="contact" />
-  <p hidden>
-    <label>Don’t fill this out: <input name="bot-field" /></label>
+        
+<form name="contact" method="POST" netlify>
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
   </p>
   <p>
-    <label>Your Name: <input type="text" name="name" required /></label>
+    <label>Your Email: <input type="email" name="email" /></label>
   </p>
   <p>
-    <label>Your Email: <input type="email" name="email" required /></label>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
   </p>
   <p>
-    <label>Your Role: 
-      <select name="role[]" multiple>
-        <option value="leader">Leader</option>
-        <option value="follower">Follower</option>
-      </select>
-    </label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message" required></textarea></label>
+    <label>Message: <textarea name="message"></textarea></label>
   </p>
   <p>
     <button type="submit">Send</button>
   </p>
 </form>
+
 
 
     </div>
