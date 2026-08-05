cf5f15da25157ab6f22f86d9b37b5d0288aed21d
Dan <hornedigital01@gmail.com>
2026-08-05 10:35:05 +0100

    Update LocateWithForm.vue

--- storyblok/Grids/LocateWithForm.vue
+++ storyblok/Grids/LocateWithForm.vue
@@ -30,7 +30,7 @@
 			<div class="row bottom-row">
 				<h3 v-if="blok?.formHeading?.length">{{ blok.formHeading }}</h3>
 
-				<h5 v-if="blok?.formSubHeading?.length">{{ blok.formSubHeading }}</h5>
+				<h6 v-if="blok?.formSubHeading?.length">{{ blok.formSubHeading }}</h6>
 				<form @submit.prevent="handleSubmit">
 					<div class="form-row">
 						<div class="form-column left">
